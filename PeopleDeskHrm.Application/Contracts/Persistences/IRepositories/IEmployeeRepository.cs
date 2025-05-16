323cfa9a6b28cf7f8c698b6294b3b377f5f237db
senhimadri <senhimadri.dev@gmail.com>
2025-05-16 09:13:39 +0600

    updated

--- PeopleDeskHrm.Application/Contracts/Persistences/IRepositories/IEmployeeRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using PeopleDeskHrm.Domain;
-using System.Linq.Expressions;
-
-namespace PeopleDeskHrm.Application.Contracts.Persistences.IRepositories;
-
-public interface IEmployeeRepository : IGenericRepository<Employee,Guid>
-{
-    IQueryable<Employee> GetEmployeesWithDetails(Expression<Func<Employee, bool>> filter);
-}
